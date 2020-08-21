306968d3c8cbfcb23427d237f950d8b41f66644a
Danny320 <2261536364@qq.com>
2020-08-21 23:01:59 +0800

    1

--- 20200812/ld/unittest.py
+++ 20200812/ld/unittest.py
@@ -5,7 +5,7 @@
 # # driver.quit()
 
 
-#无界面模式
+# 无界面模式
 # from selenium import webdriver
 # from selenium.webdriver.chrome.options import Options # 选择参数
 # options = Options()
@@ -15,8 +15,7 @@
 # driver.quit()
 
 
-
-#自动化测试元素如何定位
+# 自动化测试元素如何定位
 # import time
 #
 # from selenium import webdriver
@@ -34,21 +33,35 @@
 
 
 # 表单   自动化测试工具selenium-->表单上面
-from selenium import webdriver
-import time
-driver = webdriver.Chrome()
-driver.get('https://mail.163.com/')
-# 浏览器解析服务端返回的数据需要时间
-driver.implicitly_wait(5)
-form = driver.find_element_by_id('x-URS-iframe')
-driver.switch_to.frame(form)
-driver.find_element_by_name('email').send_keys('DannyL0320')
-driver.find_element_by_name('password').send_keys('0320dan')
-time.sleep(6)
+import ddt as ddt
+# from selenium import webdriver
+# import time
+# driver = webdriver.Chrome()
+# driver.get('http://www.baidu.com')
+# # 浏览器解析服务端返回的数据需要时间
+# driver.implicitly_wait(5)
+# form = driver.find_element_by_id('x-URS-iframe')
+# driver.switch_to.frame(form)
+# driver.find_element_by_name('email').send_keys('DannyL0320')
+# driver.find_element_by_name('password').send_keys('0320dan')
+# time.sleep(6)
 
 
+# ddt 使用
+import unittest
+from ddt import ddt, data, file_data, unpack
 
 
-
-
+@ddt
+class FooTestCase(unittest.TestCase):
+    from selenium import webdriver
+    import time
+    driver = webdriver.Chrome()
+    driver.get('http://www.baidu.com')
+    @data("lisa", "张杰", "杨幂", "杨颖")
+    def test_baidu2(self, value):
+        driver = self.driver
+        driver.get(self.base_url + "/")
+        driver.find_element_by_id("kw").send_keys(value)
+        driver.find_element_by_id("su").click()
 
